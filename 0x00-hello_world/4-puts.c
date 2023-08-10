c0cdd450af8c030353bdb3b5c4ab6366a7d6d1fb
Younik16 <okonkwogetty@gmail.com>
2023-08-10 13:09:32 -0700

    outputing C language with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-/* This prints a line in C using puts */
+/** main - entry point
+ *  This prints a line in C using puts
+ *  Return 0 (success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
+	puts("Programming is like building a multilingual puzzle\n");
+         return (0);
 }
