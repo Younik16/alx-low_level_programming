3ddc567640ab9417bd3e4b1e13b640e946c05446
Younik16 <okonkwogetty@gmail.com>
2023-08-10 12:43:17 -0700

    This is a unix header file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <unistd.h>
+/* This is a Unix header for C programming language */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
