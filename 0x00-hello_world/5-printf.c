0a264d8a0c6a4826ac209b83a644e95431fdc1e0
Younik16 <okonkwogetty@gmail.com>
2023-08-10 13:54:36 -0700

    outputing C language with puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
+ * main - Entry point
  * This project prints with the printf output style of writting
- * the task name is 5-printf.c
+ * return 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
