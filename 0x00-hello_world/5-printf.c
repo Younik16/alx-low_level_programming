8a182c4aa411deac5a164f3fbb7fda3984eab84d
Younik16 <okonkwogetty@gmail.com>
2023-08-10 11:39:03 -0700

    outputing in C with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * This project prints with the printf output style of writting
+ * the task name is 5-printf.c
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
