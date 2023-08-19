4ed16acae11471b504c1077df86671fc3c8b7317
Younik16 <okonkwogetty@gmail.com>
2023-08-19 08:35:29 -0700

    C program to print 0 to 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,22 +9,14 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 1 ; 1 <= 10 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
 		for (j = 0 ; j <= 14 ; j++)
 		{
-			putchar(j + '0');
+			if (j >= 10)
+				putchar('1');
+			putchar(j % 10 + '0');
 		}
 	putchar('\n');
 	}
-}
-/**
- *  main - to return function
- *  Return: 0
- */
-
-int main(void)
-{
-	more_numbers();
-	return (0);
 }
