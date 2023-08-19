9efd8aa711923a2a5e08207dece4d9a7b8c4958e
Younik16 <okonkwogetty@gmail.com>
2023-08-19 13:16:28 -0700

    C program to print FizzBuzz and numbers

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  main - to check the code
+ *  program to print fizz buzz and numbers
+ *  Return: numbers and fizz buzz
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+			printf("Fizz ");
+		else if (i % 5 == 0 && i % 3 != 0)
+			printf("Buzz ");
+		if (i % 5 == 0 && i % 3 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+	}
+		printf("\n");
+		return (0);
+}
