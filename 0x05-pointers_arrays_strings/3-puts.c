caeba5c04aee4a4d688f70282180c12d700049ac
Younik16 <okonkwogetty@gmail.com>
2023-08-21 19:25:13 -0700

    C program using puts to print string followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts - C program to print string followed by a new line
+ * @str: pointer function parameter
+ * Return: string
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,4 +2,6 @@
 #define MAIN_H
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
+int _strlen(char *s);
+void _puts(char *str);
 #endif
