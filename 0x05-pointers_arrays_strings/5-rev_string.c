9248a6cd918096ed7ed3df03e194c6596b66ebb9
Younik16 <okonkwogetty@gmail.com>
2023-08-22 01:10:23 -0700

    C program to print in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,9 +9,23 @@
 
 void rev_string(char *s)
 {
-	int i;
+	int count = 0, i, j;
+	char *str, temp;
 
-	for (i = strlen(s) - 1 ; i >= 0 ; i--)
-		printf("%c", s[i]);
-	printf("\n");
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
+	str = s;
+	for (i = 0; i < (count - 1); i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
+		}
+	}
 }
