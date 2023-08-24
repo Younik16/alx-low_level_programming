e2386dd6848eaf667350c739e8dbe8a126daaa44
Younik16 <okonkwogetty@gmail.com>
2023-08-24 06:05:12 -0700

    C program to capitalize all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,18 +7,22 @@
  */
 char *cap_string(char *str)
 {
-    int i = 0;
+	int j, k;
+	char a[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 9, 10};
 
-    while (str[i] != '\0') {
-        if (i == 0 || !isalpha(str[i - 1])) {
-            str[i] = toupper(str[i]);
-        } else {
-            str[i] = tolower(str[i]);
-        }
-
-        i++;
-    }
-
-    return str;
+	for (j = 0 ; str[j] != '\0' ; j++)
+	{
+		if (j == 0 && str[j] >= 97 && str[j] <= 122)
+		{
+			str[j] = str[j] - 32;
+		}
+	}
+	for (k = 0 ; a[k] != '\0' ; k++)
+	{
+	if (a[k] == str[j] && (str[j + 1] >= 97 && str[j + 1] <= 122))
+	{
+	str[j + 1] = str[j + 1] - 32;
+	}
+	}
+	return (str);
 }
-
