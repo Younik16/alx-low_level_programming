b474a5cd65433affc001d08b66b570f2658ffb29
Younik16 <okonkwogetty@gmail.com>
2023-08-28 09:30:27 -0700

    C program to locate a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr - C program to locate a character in a string
+ * @s: string
+ * @c: character
+ */
+
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
