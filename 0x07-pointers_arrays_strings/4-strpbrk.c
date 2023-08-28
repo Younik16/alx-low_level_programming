1301a9731035d91d9524e542b10e2bd756dfbe22
Younik16 <okonkwogetty@gmail.com>
2023-08-28 09:54:00 -0700

    C program to search a string for any set of byte

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strpbrk - C program to search a string for any of a set of byte
+ * @s: string
+ * @accept: strin
+ * Return: byte
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
