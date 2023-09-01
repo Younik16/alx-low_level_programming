4d0ab7bd5afb6a65a9bc6784701fd067ea0a737b
Younik16 <okonkwogetty@gmail.com>
2023-09-01 13:53:02 -0700

    C program to print minimum number of coins to make change for an amount

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *main - prints the minimum number of coins to make change for an amount of money.
+ *main - prints minimum number of coins to make change for an amount
  *@argc: argument count
  *@argv: argument vector
  *Return: 0
@@ -9,20 +9,40 @@
 
 int main(int argc, char *argv[])
 {
-	int a, b= 0;
+	int a;
+	int b = 0;
 
 	if (argc != 2)
 	{
 	printf("Error\n");
-return (1);
+	return (1);
 	}
-a = atoi(argv[1])
-while (a > 0)
-{
-	b++;
-if((a - 25) >= 0)
-{
-a -= 25;
-continue;
+	a = atoi(argv[1]);
+		while (a > 0)
+		{
+			b++;
+			if ((a - 25) >= 0)
+			{
+				a -= 25;
+				continue;
+			}
+			if ((a - 10) >= 0)
+			{
+				a -= 10;
+				continue;
+			}
+			if ((a - 5) >= 0)
+			{
+				a -= 5;
+				continue;
+			}
+			if ((a - 2) >= 0)
+			{
+				a -= 2;
+				continue;
+			}
+			a--;
+		}
+	printf("%d\n", b);
+	return (0);
 }
-if
