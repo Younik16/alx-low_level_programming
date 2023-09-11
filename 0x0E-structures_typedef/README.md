d1a1927581578623d5020d57ed7e988a4eb7e597
Younik16 <okonkwogetty@gmail.com>
2023-09-11 12:03:56 -0700

    C program to define a struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+C projects on structures and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - C function to create a struct
+ * @name: struct 1
+ * @age: struct 2
+ * @owner: struct 3
+ * Return: struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
